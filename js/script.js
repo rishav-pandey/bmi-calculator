c9c86930796275d9223da6bf78fcb8fb93e686bf
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-09-07 02:11:26 +0530

    completed code

---
+++ js/script.js
@@ -0,0 +1,74 @@
+function calculate () {
+  const height = $('#height-input').val();
+  const weight = $('#weight-input').val();
+  const heightUnit = $('#height-unit').val();
+  const weightUnit = $('#weight-unit').val();
+  newHeight = parseInt((heightConversion(heightUnit, height)));
+  newWeight = parseInt((weightConversion(weightUnit, weight)));
+  console.log(newHeight, newWeight);
+  bmiCalculate(newHeight,newWeight);
+
+}
+
+function heightConversion (heightUnit, height) {
+  switch (heightUnit) {
+    case "cm":
+      height = height;
+      break;
+    case "m":
+      height = height * 100;
+      break;
+    case "ft":
+      height = height * 30.48;
+      break;
+    case "in":
+      height = height * 2.54;
+      break;
+    default:
+      alert("Unidentified Unit");
+      break;
+  }
+  return height;
+}
+
+function weightConversion (weightUnit, weight) {
+  switch (weightUnit) {
+    case "kg":
+      weight = weight;
+      break;
+    case "lbf":
+      weight = weight * 0.453592;
+      break;
+    case "gm":
+      weight = weight * 0.001;
+      break;
+    default:
+      alert("Unidentified Unit");
+      break;
+  }
+  return weight;
+}
+
+function bmiCalculate(height, weight) {
+  if (height && weight) {
+    const bmi = (weight / (height * height) ) * 10000;
+
+    $('#display').text(bmi.toFixed(2));
+  }
+  else {
+    alert("First Fill all the Field to cotinue");
+    $('#height-input').focus();
+  }
+}
+
+function bmiStatus () {
+  // body...
+}
+
+
+
+$('#submit-button').click(function(event) {
+  /* Act on the event */
+  event.preventDefault();
+  calculate();
+});
